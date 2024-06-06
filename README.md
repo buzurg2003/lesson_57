f3fba91876043747bd00a829fef8d7628ea2fc7f
Programmer_2003 <buzurgcoder2003@gmail.com>
2024-06-06 19:13:14 +0500

    Updated: README.md and groups_widget.dart was modified

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# lesson_57
+# lesson_57 | LazyLoad Dart & Flutter.
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## ToDo Flutter App | Hive
+### [Course link](https://www.youtube.com/watch?v=EO7PDDHHYUE&list=PLrnbjo4fMQwbLEFI7EjsL8kEVMjlbpCfH&index=3)
--- lib/widgets/groups/groups_widget.dart
+++ lib/widgets/groups/groups_widget.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
-class GroupsWidget extends StatefulWidget {
+class GroupsWidget extends StatelessWidget {
   const GroupsWidget({super.key});
 
   @override
-  State<GroupsWidget> createState() => _GroupsWidgetState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Groups'),
+      ),
+      body: const _GroupsListWidget(),
+    );
+  }
 }
 
-class _GroupsWidgetState extends State<GroupsWidget> {
+
+class _GroupsListWidget extends StatefulWidget {
+  const _GroupsListWidget({super.key});
+
+  @override
+  State<_GroupsListWidget> createState() => _GroupsListWidgetState();
+}
+
+class _GroupsListWidgetState extends State<_GroupsListWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: 10, 
+      separatorBuilder: (BuildContext context, int index) {
+        return const _GroupsListRowWidget();
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return const Divider(height: 1);
+      },
+    );
+  }
+}
+
+class _GroupsListRowWidget extends StatelessWidget {
+  const _GroupsListRowWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile(
+      title: Text('Text'),
+      trailing: Icon(Icons.chevron_right),
+    );
   }
 }
